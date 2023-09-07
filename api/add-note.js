40a3173e0ecf5957732a847dfd1a53db473d4fc6
OlawaleP <99323873+OlawaleP@users.noreply.github.com>
2023-09-07 14:54:29 +0100

    updated

--- api/add-note.js
+++ api/add-note.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'us-east-1' });
 const moment = require('moment');
-const uuidv4 = require('uuid/v4');
+const uuidv4 = require('uuid');
 
 const util = require('./utils');
 
